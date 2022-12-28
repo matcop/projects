058f401a1defbb8b9800e12fbddec47664bb2268
unknown <matcop@gmail.com>
2022-12-27 20:30:27 -0400

    login mas proteccion coors

--- src/app.js
+++ src/app.js
@@ -3,8 +3,30 @@ import projectRoutes from "./routes/projects.routes.js"
 import taskRoutes from "./routes/tasks.routes.js"
 import userRoutes from "./routes/users.routes.js"
 import deviceRoutes from "./routes/device.routes.js"
+import cors from "cors";
 
 const app = express();
+
+const whiteList=[process.env.ORIGIN1]//registrar todos los dominios qu se realcionan con el backend
+// app.use(cors()); // TODOS PUEDEN ACCEDER NUESTRO SITIO
+app.use(
+    cors({
+        origin: function (origin, callback) {
+           // console.log("😲😲😲 =>", origin);
+            // if (whiteList.includes(origin)) {
+                if (!origin || whiteList.includes(origin)) {
+                return callback(null, origin);
+            }
+            return callback(
+                "Error de CORS origin: " + origin + " No autorizado!⁜⁜⁜⁜"
+            );
+        },
+        credentials: true,
+    })
+);
+
+
+
 app.use(express.json())
 //app.use(express.json())
 app.use (projectRoutes)
